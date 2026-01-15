0c65e65bfd57ba9d2c3d4726c56314b7c1375bad
Waylon DeLore <waylon.delore@crpusd.org>
2026-01-15 10:03:55 -0800

    Output shows up in window and buttons create proper output

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -9,9 +9,22 @@ from tkinter.filedialog import asksaveasfilename
 
 
 def do_command(command):
-    global command_textbox
-    subprocess.call("ping localhost")
-global command_textbox, url_entry
+    global command_textbox, url_entry
+
+    # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        # url_val = "127.0.0.1"
+        url_val = "::1"
+    
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+
+    with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            command_textbox.insert(tk.END,line)
+            command_textbox.update()
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
@@ -32,11 +45,11 @@ ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", comma
 ping_btn.pack()
 
 # tracert button
-tracert_btn = tk.Button(frame, text="Tracert Button", command=lambda:do_command("tracert"))
+tracert_btn = tk.Button(frame, text="Tracert", command=lambda:do_command("tracert"))
 tracert_btn.pack()
 
 # nslookup button
-nslookup_btn = tk.Button(frame, text="Nslookup Button", command=lambda:do_command("nslookup"))
+nslookup_btn = tk.Button(frame, text="Nslookup", command=lambda:do_command("nslookup"))
 nslookup_btn.pack()
 
 # creates the frame with label for the text box
@@ -49,8 +62,7 @@ url_label = tk.Label(frame_URL, text="Enter a URL of interest: ",
     font=("comic sans", 14),
     bd=0, 
     relief=tk.FLAT, 
-    cursor="heart",
-    fg="mediumpurple3",
+    fg="white",
     bg="black")
 url_label.pack(side=tk.LEFT)
 url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
